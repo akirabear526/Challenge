98d9a334ab8afbfc668d65456cb8a282b1630aa4
久万顕 <>
2018-05-21 11:01:13 +0900

    java課題バイト数と文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge29.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author Akirabear
+ */
+ class challenge29 {
+     public static void main(String[] args){
+         String name = "クマアキラ";
+         
+         System.out.println(name.getBytes().length);
+         System.out.println(name.length());
+     }
+    
+}
